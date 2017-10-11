b0f874f5fbdea9f1ed569f73c2881de5435a0a72
Euan Rochester <euan@rochester.me.uk>
2017-10-11 22:57:44 +0100

    Improve cache error handling & allow retrys

--- src/cache.rs
+++ src/cache.rs
@@ -1,5 +1,6 @@
 use download::{self, DownloadManager};
 use futures::future;
+#[macro_use]
 use futures::prelude::*;
 use hyper::{self, Uri};
 use slog::Logger;
@@ -18,7 +19,7 @@ pub trait Cache<T: Cacheable + 'static> {
         t.cached_path().exists()
     }
 
-    fn with(t: &T, manager: DownloadManager, log: Logger) -> download::BoxFuture<PathBuf>;
+    fn with(t: T, manager: DownloadManager, log: Logger) -> download::BoxFuture<PathBuf>;
 
     #[async(boxed)]
     fn install_at(t: T,
@@ -26,7 +27,7 @@ pub trait Cache<T: Cacheable + 'static> {
                   manager: DownloadManager,
                   log: Logger)
                   -> ::download::Result<()> {
-        let cached_path = await!(Self::with(&t, manager, log.clone()))?;
+        let cached_path = await!(Self::with(t, manager, log.clone()))?;
         info!(log, "installing item"; "location"=>location.as_path().to_string_lossy().into_owned());
 
         fs::create_dir_all(&location)?;
@@ -37,32 +38,40 @@ pub trait Cache<T: Cacheable + 'static> {
     }
 }
 
-fn first_file_in_folder<P: AsRef<Path>>(path: P) -> PathBuf {
-    path.as_ref()
+fn first_file_in_folder<P: AsRef<Path>>(path: P) -> Result<PathBuf,download::Error> {
+    Ok(path.as_ref()
         .read_dir()
-        .expect("cache folder was unreadable")
+        .map_err(|_| download::Error::from(download::ErrorKind::CacheError))?
         .next()
-        .expect("cache folder was empty")
+        .ok_or_else(|| download::Error::from(download::ErrorKind::CacheError))?
         .unwrap()
-        .path()
+        .path())
 }
 
 pub struct FolderCache;
 
 impl<T: Cacheable + 'static> ::cache::Cache<T> for FolderCache {
-    fn with(t: &T, manager: DownloadManager, log: Logger) -> download::BoxFuture<PathBuf> {
+    fn with(t: T, manager: DownloadManager, log: Logger) -> download::BoxFuture<PathBuf> {
         let cached_path = t.cached_path();
         let log = log.new(o!("cached_path"=>cached_path.as_path().to_string_lossy().into_owned()));
 
-        if Self::is_cached(t) {
+        if Self::is_cached(&t) {
             info!(log, "item was already cached");
-            Box::new(future::ok(first_file_in_folder(cached_path)))
+            Box::new(future::result(first_file_in_folder(&cached_path)).or_else(move |_|{
+                //invalidate cache
+                warn!(log,"Removing invalid cache folder {:?}",cached_path);
+                future::result(fs::remove_dir(cached_path).map_err(|ioe| download::Error::from(ioe))).and_then(|_|{
+                    //FIXME: will retry forever
+                    //retry
+                    Self::with(t,manager,log)
+                })
+            }))
         } else {
             info!(log, "item is not cached, downloading now");
             match t.uri() {
                 Ok(uri) => {
                     Box::new(manager.download(uri, cached_path.clone(), true, log)
-                        .map(move |_| first_file_in_folder(cached_path)))
+                        .and_then(move |_| first_file_in_folder(cached_path)))
                 }
                 Err(e) => Box::new(future::err(download::Error::from(e))),
             }
@@ -73,11 +82,11 @@ impl<T: Cacheable + 'static> ::cache::Cache<T> for FolderCache {
 pub struct FileCache;
 
 impl<T: Cacheable + 'static> ::cache::Cache<T> for FileCache {
-    fn with(t: &T, manager: DownloadManager, log: Logger) -> download::BoxFuture<PathBuf> {
+    fn with(t: T, manager: DownloadManager, log: Logger) -> download::BoxFuture<PathBuf> {
         let cached_path = t.cached_path();
         let log = log.new(o!("cached_path"=>cached_path.as_path().to_string_lossy().into_owned()));
 
-        if Self::is_cached(t) {
+        if Self::is_cached(&t) {
             info!(log, "item was already cached");
             Box::new(future::ok(cached_path))
         } else {
